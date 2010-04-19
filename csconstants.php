84a2b8cd2c8751a07eaeb3e092e466ddd3026486
graeme@cloudsponge.com <graeme@cloudsponge.com@9b972813-1449-f663-43eb-a48a7bd4afe1>
2010-04-19 19:28:47 +0000

    initial commit
    git-svn-id: https://cloudsponge-lib-php.googlecode.com/svn/trunk@2 9b972813-1449-f663-43eb-a48a7bd4afe1

---
+++ csconstants.php
@@ -0,0 +1,18 @@
+<?php
+
+// CloudSponge.com PHP Library v0.9.2 Beta
+// http://www.cloudsponge.com
+// Copyright (c) 2010 Cloud Copy, Inc.
+// Licensed under the MIT license: http://www.opensource.org/licenses/mit-license.php
+//
+// Written by Graeme Rouse
+// graeme@cloudsponge.com
+
+/* Global Configuration Settings */
+// The simple inteface below defines contacts that identify your application to CloudSponge.com.
+interface iCSConstants {
+  // DOMAIN_KEY and DOMAIN_PASSWORD should contain your domain key and domain password for CloudSponge.com access.
+ const DOMAIN_KEY = "CHANGEME";
+ const DOMAIN_PASSWORD = "CHANGEME";
+}
+?>
