bae31bd362a0e937c00e7643ed30b818c2cf5057
graeme@cloudsponge.com <graeme@cloudsponge.com@9b972813-1449-f663-43eb-a48a7bd4afe1>
2011-03-07 20:01:12 +0000

    updated applet tag to work across major browsers.
    git-svn-id: https://cloudsponge-lib-php.googlecode.com/svn/trunk@9 9b972813-1449-f663-43eb-a48a7bd4afe1

--- csimport.php
+++ csimport.php
@@ -186,11 +186,34 @@ class CSImport implements iCSConstants {
   }
   static function create_applet_tag($id, $url) {
     return <<<EOS
-<APPLET archive="$url" code="ContactsApplet" id="Contact_Importer" width="0" height="0">
-  <PARAM name="cookieValue" value="document.cookie"/>
-  <PARAM name="importActionID" value="$id"/>
-  Your browser does not support Java which is required for this utility to operate correctly.
-</APPLET>
+<!--[if !IE]> Firefox and others will use outer object -->
+<object classid="java:ContactsApplet" type="application/x-java-applet" archive="$url" height="1" width="1">
+  <!-- Konqueror browser needs the following param -->
+  <param name="archive" value="$url" />
+  <param name="importId" value="$id"/>
+  <param name="frameName" value="cs_container_frame"/>
+  <!--<![endif]-->
+  <!-- MSIE (Microsoft Internet Explorer) will use inner object --> 
+  <!-- the latest autodownload links for java found here: http://www.oracle.com/technetwork/java/javase/autodownload-140472.html -->
+  <object classid="clsid:8AD9C840-044E-11D1-B3E9-00805F499D93" codebase="http://java.sun.com/update/1.6.0/jinstall-6u24-windows-i586.cab" height="0" width="0"> 
+    <param name="code" value="ContactsApplet" />
+    <param name="archive" value="$url" />
+    <param name="importId" value="$id"/>
+    <param name="frameName" value="cs_container_frame"/>
+    <applet archive="$url" code="ContactsApplet" height="1" width="1">
+      <param name="importId" value="$id"/>
+      <param name="frameName" value="cs_container_frame"/>
+      <strong>
+        This browser does not have a Java Plug-in.<br/>
+        Get the latest Java Plug-in <a href="http://javadl.sun.com/webapps/download/AutoDL?BundleId=47376" onclick="startOver(); return true;">here</a>.
+      </strong>
+    </applet>
+  </object> 
+<!--[if !IE]> close outer object -->
+</object>
+<!--<![endif]-->
+<!-- The latest links to the installer can be found here: http://java.com/en/download/ -->
+<!-- Get the latest Java Plug-in <a href="http://javadl.sun.com/webapps/download/GetFile/1.6.0_24-b07/windows-i586/xpiinstall.exe">here</a>. -->
 EOS;
   }
   static function generate_poll_url($path, $import_id) {
