76e0bf3c1a89325c62ea20689abb4186eb7649bc
graeme@arizonabay.com <graeme@arizonabay.com@9b972813-1449-f663-43eb-a48a7bd4afe1>
2011-06-20 19:16:42 +0000

    added call to exit; in auth.php on Chris McKeever of GiveForward.com's finding that it fixes a Kohana integration issue.
    git-svn-id: https://cloudsponge-lib-php.googlecode.com/svn/trunk@12 9b972813-1449-f663-43eb-a48a7bd4afe1

--- auth.php
+++ auth.php
@@ -11,8 +11,20 @@
 require_once 'csimport.php';
 $response = CSImport::forward_auth($_GET, $_POST);
 if (isset($response['redirect'])) {
+  // This is our successful case
+  // calling header(..) only works if no body 
+  // content has been sent yet. If you are integrating this
+  // page in a web application framework, 
+  // you may need to redirect differently. 
+  // E.g. With Kohana: 
+  //  url::redirect($response['redirect']);
   header($response['redirect']);
 } else {
+  // We shouldn't get here
   echo $response['body'];
 }
+
+// calling exit here to ensure that the response is sent, event if there is no body content. 
+// Thanks to Chris McKeever of GiveForward.com for discovering this issue when integrating with Kohana. 
+exit;
 ?>
